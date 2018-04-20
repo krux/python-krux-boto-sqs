45907d8c58ef5ddf79df2568a5c3bb17fc52af69
Peter Han <phan@salesforce.com>
2018-04-19 22:55:30 -0700

    Tests for __init__() and _get_queue()

--- tests/sqs_test.py
+++ tests/sqs_test.py
@@ -11,23 +11,78 @@ from __future__ import absolute_import
 import unittest
 
 #
+# Third party libraries
+#
+
+from mock import MagicMock, patch
+import boto3
+
+#
 # Internal libraries
 #
 
-import krux_boto.boto
-import krux_sqs.sqs
-
+from krux_boto.boto import Boto3
+from krux_sqs.sqs import Sqs
 
 class SqsTest(unittest.TestCase):
     TEST_REGION = 'us-west-2'
     TEST_QUEUE_NAME = 'seth-test-1'
 
     def setUp(self):
-        self._sqs = krux_sqs.sqs.Sqs(
-            boto=krux_boto.boto.Boto3(
-                region=self.TEST_REGION
-            )
+        self._logger = MagicMock()
+        self._stats = MagicMock()
+
+        self._resource = MagicMock()
+        self._boto = MagicMock(
+            spec=Boto3,
+            resource=MagicMock(return_value=self._resource),
         )
+
+        self._sqs = Sqs(
+            boto=self._boto
+        )
+
+    def test_init(self):
+        """
+        Sqs.__init__() correctly initialize internal fields
+        """
+        self.assertEqual(self._resource, self._sqs._resource)
+        self._boto.resource.assert_called_once_with('sqs')
+        self.assertEqual({}, self._sqs._queues)
+
+    def test_init_not_implemented(self):
+        """
+        Sqs.__init__() correctly raise an error for non-supported Boto objects
+        """
+        with self.assertRaises(NotImplementedError):
+            Sqs(
+                boto=MagicMock()
+            )
+
+    def test_get_queue_no_cache(self):
+        """
+        Sqs._get_queue() correctly creates and caches the queue upon the first call
+        """
+        self._sqs._queues = {}
+        expected = self._resource.get_queue_by_name.return_value
+
+        self.assertEqual(expected, self._sqs._get_queue(SqsTest.TEST_QUEUE_NAME))
+
+        self.assertEqual(expected, self._sqs._queues.get(SqsTest.TEST_QUEUE_NAME))
+        self._resource.get_queue_by_name.assert_called_once_with(QueueName=SqsTest.TEST_QUEUE_NAME)
+
+    def test_get_queue_cache(self):
+        """
+        Sqs._get_queue() correctly uses the cached queue object
+        """
+        expected = self._resource.get_queue_by_name.return_value
+        self._sqs._queues = {
+            SqsTest.TEST_QUEUE_NAME: expected
+        }
+
+        self.assertEqual(expected, self._sqs._get_queue(SqsTest.TEST_QUEUE_NAME))
+
+        self.assertFalse(self._resource.get_queue_by_name.called)
 
     @unittest.skip("Test queue missing. Skip for now.")
     def test_get_messages(self):
